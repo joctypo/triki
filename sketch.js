95162cbd963f6aa3f5c7da03fb968329e4d3c770
Julian <usuario@MacBook-Pro-de-USUARIO-3.local>
2020-02-07 15:41:03 -0500

    tercerojulian

--- sketch.js
+++ sketch.js
@@ -8,13 +8,33 @@ function setup(){
 
 forms=[];
 createCanvas(600,600);
-background(0);
+
 }
 function draw(){
+    background(100);
+    fill(20);
+line(0,200,600,200);
+line(0,400,600,400);
+line(200,0,200,600);
+line(400,0,400,600);
 
 }
 function mouseClicked(){
 
-if(mouseX)
+    for(let i = 0; i < forms.size(); i++){
+
+        if(mouseX){
+
+    }
+
+    function pintarForma(){
+
+        
+    }
+
+
+
+
+}
 
 }
