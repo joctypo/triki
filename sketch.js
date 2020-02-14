f1c5a79bd650a527444c35b37ac82d955f026b52
joctypo <oscargiraldop1999@gmail.com>
2020-02-14 10:56:56 -0500

    doce oscar

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ let matriz = [
     [0,0,0],
 ];
 let turno;
+let posicion1;
+let posicion2;
 
 function setup(){
     createCanvas(600,600);
@@ -17,24 +19,44 @@ function setup(){
 function draw(){
     background(100);
     fill(255);
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            rect(200*i,200*j,200,200);
+        }
+    }
     for (let i = 0; i < forms.length; i++) {
         forms[i].dibujar();
     }
-
 fill(20);
 line(0,200,600,200);
 line(0,400,600,400);
 line(200,0,200,600);
 line(400,0,400,600);
 }
-function mouseClicked() {
-    
-    if (turno==false){
-        forms.push(new circulo(100,100));
-    }else if (turno==true){
-        forms.push(new X(100,100));
-    }
 
+
+
+
+function mouseClicked() {
+    posicion1= parseInt(mouseX/200);
+    posicion2= parseInt(mouseY/200);
+    
+    if(matriz[posicion1][posicion2]==0){
+        if (turno==false){
+            forms.push(new circulo(100,100));
+            turno=true;
+        }else if (turno==true){
+            forms.push(new X(100,100));
+            turno=false;
+        }
+
+    }
+   
+
+
+}
+
+function evaluarclick() {
     //primera fila
     if(mouseX>0 && mouseX<200 && mouseY>0 && mouseY<200){
         valor;
@@ -65,9 +87,5 @@ function mouseClicked() {
     if(mouseX>400 && mouseX<600 && mouseY>400 && mouseY<600){
         valor;
     }
-    for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 3; j++){
-            rect(200*i,200*j,200,200);
-        }
-    }
+
 }
