cb928346e5897b3fb8c3fba6b7f4d0b4b30caa37
joctypo <oscargiraldop1999@gmail.com>
2020-02-14 10:17:53 -0500

    quinto Oscar

--- circulo.js
+++ circulo.js
@@ -1,8 +1,10 @@
 class Circulo extends Formas{
-    constructor(posX,posY,tamX,tamY){
-        super(posX,posY,tamX,tamY);
+    super(posX,posY){
     }
+    
     pintarCirculo(){
-        
-    }
+    fill(255,0,0);
+    ellipse(this.posX,this.posY,100,100);
+}
+
 }
